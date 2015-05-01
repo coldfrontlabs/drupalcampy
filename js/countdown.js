3faea6d414efb755fe3595f88042c87d15b51e86
Tiffany Tse <ttse@coldfrontlabs.ca>
2015-05-01 22:35:16 +0000

    refactor(js): update countdown time

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 var current = "Conference Is Now"; //-->enter what you want the script to display when the target date and time are reached, limit to 20 characters
 var year = 2015; //-->Enter the count down target date YEAR
 var month = 6; //-->Enter the count down target date MONTH
-var day = 16; //-->Enter the count down target date DAY
-var hour = 0; //-->Enter the count down target date HOUR (24 hour clock)
+var day = 24; //-->Enter the count down target date DAY
+var hour = 8; //-->Enter the count down target date HOUR (24 hour clock)
 var minute = 0; //-->Enter the count down target date MINUTE
 var tz = -5; //-->Offset for your timezone in hours from UTC
 
