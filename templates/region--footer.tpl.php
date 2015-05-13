08fd63f5d9cb60e192f1303a329677ff9e7de7cb
Tiffany Tse <ttse@coldfrontlabs.ca>
2015-05-13 09:30:11 +0000

    feat(sass): add styles for news page

--- templates/region--footer.tpl.php
+++ templates/region--footer.tpl.php
@@ -11,6 +11,7 @@
   <footer id="footer" class="<?php print $classes; ?>">
     <div class="full-width-wrapper">
     <?php print $content; ?>
+    <div class="colophon"><?php print t('Copyright &copy; ') . date(Y) . t(' DrupalCamp Ottawa. Built using the !cod', array('!cod' => l('Conference Organizing Distribution. ', 'https://www.drupal.org/project/cod', array('external' => TRUE)))); ?></div>
     </div>
   </footer>
 <?php endif; ?>
